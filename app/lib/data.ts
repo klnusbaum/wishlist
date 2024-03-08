e44fb6255007573338861e47adcd4d1b67d442da
Kurtis Nusbaum <kcommiter@gmail.com>
2024-03-07 21:08:48 -0800

    Add database functions for wishlist

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,27 +1,26 @@
 import { sql } from '@vercel/postgres';
 import {
-WishListItem
+    WishListItem
 } from './definitions';
 import { unstable_noStore as noStore } from 'next/cache';
-export async function fetchWishListItems(userId : string) {
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore();
 
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+export async function fetchWishListItems(userId: string) {
+    noStore();
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    try {
+        const data = await sql<WishListItem>`SELECT * FROM wishListItems where userId = ${userId};`;
+        return data.rows;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch revenue data.');
+    }
+}
 
-    const data = await sql<WishListItem>`SELECT * FROM wishListItems where userId = ${userId};`;
+export async function insertWishListItem(userId: string, name: string, url: string, price: number, imgURL?: string) {
 
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
+    await sql`
+      INSERT INTO wishListItems (userId, url, imgURL, name, price)
+      VALUES (${userId}, ${url}, ${imgURL}, ${name}, ${price})
+    `;
+    return 0
 }
