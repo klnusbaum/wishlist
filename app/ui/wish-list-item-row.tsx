989f6d06edf1e2efd233dbfbb7d3d4082b04e0dc
Kurtis Nusbaum <kcommiter@gmail.com>
2024-03-07 21:45:52 -0800

    fix price formatting

--- app/ui/wish-list-item-row.tsx
+++ app/ui/wish-list-item-row.tsx
@@ -5,9 +5,16 @@ export default function WishListItemRow({ item }: { item: WishListItem }) {
         <div className="h-20 w-20 min-w-20 bg-slate-300 m-4 rounded-lg"></div>
         <div className="grow self-stretch flex flex-col justify-evenly">
             <div className="text-lg">{item.name}</div>
-            <div className="text-md">${item.price}</div>
+            <div className="text-md">{formatPrice(item.price)}</div>
         </div>
         <div className="bg-slate-600 m-2 rounded size-12 md:size-16 grid justify-center items-center"><i className="gg-trash list-icon-size" ></i></div>
         <div className="bg-green-600 m-2 rounded size-12 md:size-16 grid justify-center items-center"><i className="gg-check-o list-icon-size"></i></div>
     </div >
+}
+
+function formatPrice(amount: number) {
+    return (amount / 100).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
 }
