1b400852a1b45e490331df09d4d223ba1f312ebe
Kurtis Nusbaum <kcommiter@gmail.com>
2024-03-07 21:08:31 -0800

    format wish list page

--- app/wish/list/page.tsx
+++ app/wish/list/page.tsx
@@ -1,7 +1,6 @@
 import WishListItemRow from "@/app/ui/wish-list-item-row";
 import { fetchWishListItems } from "@/app/lib/data";
 
-
 export default async function Page() {
     const items = await fetchWishListItems('410544b2-4001-4271-9855-fec4b6a6442a');
     return <div>
